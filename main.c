da88bd6db9b5972a5a6f77d0fe458e7049525b23
ArtemisPlayer <dovahkin60@gmail.com>
2021-02-18 20:18:56 +0100

    Added Demo

--- main.c
+++ main.c
@@ -473,12 +473,67 @@ void clearEcran(SDL_Renderer *renderer){
 //--------------------------------------------MAIN
 
 
+void processEvent(SDL_Event event, camera *camera, int *continuer, int *isDemo){
+  vect delta; //utile pour les mvts
+  switch(event.type)
+    {
+      case SDL_QUIT:
+        *continuer = 0;
+
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym){
+          case SDLK_LSHIFT:
+            camera->position.y += 0.5;
+            break;
+          case SDLK_LCTRL:
+            camera->position.y -= 0.5;
+            break;
+          case SDLK_z:
+            delta = camera->direction;
+            multiplicationScalaire(&delta, 0.3);
+            add(&camera->position, &delta , &camera->position);
+            break;
+          case SDLK_s:
+            delta = camera->direction;
+            multiplicationScalaire(&delta, -0.3);
+            add(&camera->position, &delta , &camera->position);
+            break;
+          case SDLK_q:
+            delta = camera->u;
+            multiplicationScalaire(&delta, -0.3);
+            add(&camera->position, &delta , &camera->position);
+            break;
+          case SDLK_d:
+            delta = camera->u;
+            multiplicationScalaire(&delta, 0.3);
+            add(&camera->position, &delta , &camera->position);
+            break;
+          case SDLK_a:
+            //rotate left
+            rotate(&camera->direction, &camera->v, 0.1);
+            break;
+          case SDLK_e:
+            //rotate right
+            rotate(&camera->direction, &camera->v, -0.1);
+            break;
+          case SDLK_SPACE:
+            if (*isDemo == 0){
+              *isDemo = 1;
+            } else {
+              *isDemo = 0;
+            }
+            break;
+        }
+        actualiserUV(camera);
+    }
+}
+
 int main(){
 
   //INITIALISATION
   camera camera;
-  camera.position.x = 0;
-  camera.position.y = 0;
+  camera.position.x = -5;
+  camera.position.y = 0.5;
   camera.position.z = 0;
   camera.direction.x = 1;
   camera.direction.y = 0;
@@ -487,23 +542,24 @@ int main(){
 
   SDL_Window *ecran;
   SDL_Renderer *renderer;
-  SDL_Color green = {0, 0, 255, 255};
+  SDL_Color green = {0, 255, 0, 255};
   SDL_Color red = {255, 0, 0, 255};
+  SDL_Color blue = {0, 0, 255, 255};
 
   ecran = SDL_CreateWindow("Madfihr", 20, 20, 640, 480, SDL_WINDOW_SHOWN);
   renderer = SDL_CreateRenderer(ecran, -1, SDL_RENDERER_ACCELERATED);
   //Création du monde
   triangle3D T1;
 
-  T1.A.x = 5;
+  T1.A.x = 0;
   T1.A.y = 1;
   T1.A.z = 0;
 
-  T1.B.x = 5;
+  T1.B.x = 0;
   T1.B.y = 1;
   T1.B.z = 1;
 
-  T1.C.x = 5;
+  T1.C.x = 0;
   T1.C.y = 0;
   T1.C.z = 0;
 
@@ -511,76 +567,64 @@ int main(){
 
   triangle3D T2;
 
-  T2.A.x = 5;
+  T2.A.x = 0;
   T2.A.y = 1;
   T2.A.z = 0;
 
-  T2.B.x = 6;
+  T2.B.x = 1;
   T2.B.y = 1;
   T2.B.z = 0;
 
-  T2.C.x = 5;
+  T2.C.x = 0;
   T2.C.y = 0;
   T2.C.z = 0;
 
   T2.color = green;
 
-  triangle3D monde[2];
+  triangle3D T3;
+
+  T3.A.x = 1;
+  T3.A.y = 1;
+  T3.A.z = 0;
+
+  T3.B.x = 0;
+  T3.B.y = 0;
+  T3.B.z = 0;
+
+  T3.C.x = 0;
+  T3.C.y = 1;
+  T3.C.z = 1;
+
+  T3.color = blue;
+
+  triangle3D monde[3];
   monde[0] = T1;
   monde[1] = T2;
-  int tailleMonde = 2;
+  monde[2] = T3;
+  int tailleMonde = 3;
 
   //RUN
   int continuer = 1;
-  vect delta; //utile pour les mvts
+  int isDemo = 0;
+  vect alpha; //pour la demo
   SDL_Event event;
   while (continuer){
     clearEcran(renderer);
     renderMonde(renderer, &camera, monde, tailleMonde);
-    SDL_WaitEvent(&event);
-    switch(event.type)
-    {
-      case SDL_QUIT:
-        continuer = 0;
+    if (isDemo == 1){
+      SDL_PollEvent(&event);
+    } else {
+      SDL_WaitEvent(&event);
+    }
+    processEvent(event, &camera, &continuer, &isDemo);
 
-      case SDL_KEYDOWN:
-        switch (event.key.keysym.sym){
-          case SDLK_LSHIFT:
-            camera.position.y += 0.5;
-            break;
-          case SDLK_LCTRL:
-            camera.position.y -= 0.5;
-            break;
-          case SDLK_z:
-            delta = camera.direction;
-            multiplicationScalaire(&delta, 0.3);
-            add(&camera.position, &delta , &camera.position);
-            break;
-          case SDLK_s:
-            delta = camera.direction;
-            multiplicationScalaire(&delta, -0.3);
-            add(&camera.position, &delta , &camera.position);
-            break;
-          case SDLK_q:
-            delta = camera.u;
-            multiplicationScalaire(&delta, -0.3);
-            add(&camera.position, &delta , &camera.position);
-            break;
-          case SDLK_d:
-            delta = camera.u;
-            multiplicationScalaire(&delta, 0.3);
-            add(&camera.position, &delta , &camera.position);
-            break;
-          case SDLK_a:
-            //rotate left
-            rotate(&camera.direction, &camera.v, 0.1);
-            break;
-          case SDLK_e:
-            //rotate right
-            rotate(&camera.direction, &camera.v, -0.1);
-            break;
-        }
-        actualiserUV(&camera);
+    if (isDemo == 1){
+      alpha = camera.u;
+      multiplicationScalaire(&alpha, -0.1);
+      add(&camera.position, &alpha , &camera.position);
+      rotate(&camera.direction, &camera.v, -0.0199973);
+      SDL_Delay(20);
+      actualiserUV(&camera);
     }
   }
 
